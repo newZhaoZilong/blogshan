7b69dd79e0d6bbd6b424d4ca03557413db8a9928
shange <newzhaozilong@gmail.com>
2018-10-05 00:19:17 +0800

    shixian rest api post

--- blogproject/blogjsonapp/views.py
+++ blogproject/blogjsonapp/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+# from django.views.decorators.csrf import requires_csrf_token
 # Create your views here.
 import json
 
+
+@csrf_exempt
 def getbloginfo(request):
+    print('有没有走到这一步')
     name_dict = {
         'name':'shangeblog',
         'desc':'测试返回json'
     }
 
-    return HttpResponse(json.dumps(name_dict),content_type='application/json',)
+    return HttpResponse(json.dumps(name_dict),content_type='application/json')
