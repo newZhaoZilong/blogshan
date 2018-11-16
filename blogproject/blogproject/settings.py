898e5ba23762e279341f32a610701856a12da4f8
shange <newzhaozilong@gmail.com>
2018-11-16 11:33:26 +0800

    1.见鬼了

--- blogproject/blogproject/settings.py
+++ blogproject/blogproject/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
+print('BASE_DIR',BASE_DIR)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
@@ -134,6 +134,10 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT =  os.path.join(BASE_DIR,'collect_static')
+
+print('STATIC_ROOT',STATIC_ROOT)
 
 # 这个路径对应img的上传到哪儿个文件夹下,是绝对路径
 MEDIA_ROOT = os.path.join(BASE_DIR, 'blog/static/blog/images')
